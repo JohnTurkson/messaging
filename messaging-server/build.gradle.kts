c96ba3d2a36f7815aa1d5bb510273d4bfe7266ec
John Turkson <git@johnturkson.com>
2020-11-12 03:50:54 -0800

    Replace OkHttp with Ktor

--- messaging-server/build.gradle.kts
+++ messaging-server/build.gradle.kts
@@ -7,21 +7,15 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
     implementation("com.amazonaws:aws-lambda-java-core:1.2.1")
-    implementation("com.squareup.okhttp3:okhttp:4.9.0")
-    implementation("com.johnturkson.aws-tools:aws-dynamodb-object-builder:0.0.19")
-    implementation("com.johnturkson.aws-tools:aws-dynamodb-request-builder:0.0.19")
-    implementation("com.johnturkson.aws-tools:aws-dynamodb-transforming-serializer:0.0.19")
-    implementation("com.johnturkson.aws-tools:aws-request-signer:0.0.19")
-    
-    // implementation("io.ktor:ktor-server-cio:1.4.2")
-    // implementation("io.ktor:ktor-server-core:1.4.2")
-    
-    // implementation("io.ktor:ktor-client:1.4.2")
-    // implementation("io.ktor:ktor-client-core:1.4.2")
-    // implementation("io.ktor:ktor-client-core-jvm:1.4.2")
     implementation("io.ktor:ktor-client-cio:1.4.2")
-    // implementation("io.ktor:ktor-websockets:1.4.2")
     implementation("io.ktor:ktor-client-websockets:1.4.2")
+    implementation("io.ktor:ktor-client-serialization:1.4.2")
+    implementation("com.johnturkson.aws-tools:aws-dynamodb-object-builder:0.0.25")
+    implementation("com.johnturkson.aws-tools:aws-dynamodb-request-builder:0.0.25")
+    implementation("com.johnturkson.aws-tools:aws-dynamodb-transforming-serializer:0.0.25")
+    implementation("com.johnturkson.aws-tools:aws-dynamodb-request-handler:0.0.25")
+    implementation("com.johnturkson.aws-tools:aws-request-signer:0.0.25")
+    implementation("com.johnturkson.aws-tools:aws-request-handler:0.0.25")
 }
 
 tasks.named<Jar>("jar") {
