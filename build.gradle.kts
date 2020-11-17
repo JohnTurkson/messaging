bd475970b76f401421e0b658bb59bc5c8a2ab24c
John Turkson <git@johnturkson.com>
2020-11-17 05:38:08 -0800

    Update Gradle plugin configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    kotlin("jvm") version "1.4.10" apply false
+    kotlin("plugin.serialization") version "1.4.10" apply false
+    `maven-publish`
+}
+
 allprojects {
     group = "com.johnturkson.messaging"
     version = "0.0.1"
